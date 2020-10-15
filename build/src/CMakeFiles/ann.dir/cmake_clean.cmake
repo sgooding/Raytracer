10767e6e9c078a3baa680350f6cca1c51845f559
Sean Gooding <sgooding@gmail.com>
2020-10-14 19:18:44 -0600

    cmake

---
+++ build/src/CMakeFiles/ann.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ann.dir/ANN.cpp.o"
+  "CMakeFiles/ann.dir/bd_fix_rad_search.cpp.o"
+  "CMakeFiles/ann.dir/bd_pr_search.cpp.o"
+  "CMakeFiles/ann.dir/bd_search.cpp.o"
+  "CMakeFiles/ann.dir/bd_tree.cpp.o"
+  "CMakeFiles/ann.dir/brute.cpp.o"
+  "CMakeFiles/ann.dir/kd_dump.cpp.o"
+  "CMakeFiles/ann.dir/kd_fix_rad_search.cpp.o"
+  "CMakeFiles/ann.dir/kd_pr_search.cpp.o"
+  "CMakeFiles/ann.dir/kd_search.cpp.o"
+  "CMakeFiles/ann.dir/kd_split.cpp.o"
+  "CMakeFiles/ann.dir/kd_tree.cpp.o"
+  "CMakeFiles/ann.dir/kd_util.cpp.o"
+  "CMakeFiles/ann.dir/perf.cpp.o"
+  "libann.pdb"
+  "libann.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ann.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
